95add188120adbe836843f30709d661180cdee68
Othniel Agera <otagera@gmail.com>
2022-05-05 15:56:42 +0100

    Delete build

--- build/controllers/APIController.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./API/UserController");
-require("./API/PaymentController");
-//# sourceMappingURL=APIController.js.map
--- build/interfaces/product.interface.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ProductCategories;
-(function (ProductCategories) {
-    ProductCategories["CABLES"] = "CABLES";
-})(ProductCategories || (ProductCategories = {}));
-//# sourceMappingURL=product.interface.js.map
