d6fc8da958c1744ae30af2ab7dd4b86167cd747b
Othniel Agera <otagera@gmail.com>
2022-05-20 22:08:20 +0100

    Push

---
+++ build/controllers/APIController.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./API/UserController");
+require("./API/ProductController");
+require("./API/OrderController");
+//# sourceMappingURL=APIController.js.map
